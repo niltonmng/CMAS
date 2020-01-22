f202adc7db34e65baea6b01db6baaea0a6350dd2
niltomng <niltonginani@gmail.com>
2020-01-22 15:26:41 -0300

    adding simple test suite

--- tests/Tests.java
+++ tests/Tests.java
@@ -1,6 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import cmas.Step1;
@@ -10,6 +9,7 @@ class Tests {
 	
 	@Test
 	void test() {
+		
 		Step1 s1 = new Step1();
 		Step2 s2 = new Step2();
 		
@@ -27,10 +27,24 @@ class Tests {
 		aux1 = aux1.trim();
 		aux2 = aux2.trim();
 		
-		
 		assertEquals(step_1, aux1);
 		assertEquals(step_2, aux2);
+		
+		assertEquals("1 2 CMAS 4 systems CMAS 7 8 CMAS systems 11 CMAS 13 14 CMASsystems 16 17 CMAS 19 systems", aux1);
+		assertEquals("1 2 good 4 systems CMAS 7 8 CMAS systems 11 CMAS good 14 CMASsystems 16 17 CMAS 19 systems", aux2);
 		assertEquals("CMAS: 4\n" + "systems: 3\n" + "CMASsystems: 1\n" + "good: 2\n" + "integer: 10", s2.counter(step_3));
+
+		assertNotEquals(aux1.split(" "), aux2.split(" "));
+		
+		assertTrue(step_1.equals(aux1));
+		assertTrue(step_2.equals(aux2));
+		assertTrue("CMAS: 4\nsystems: 3\nCMASsystems: 1\ngood: 2\ninteger: 10".equals(s2.counter(step_3)));
+		
+		assertFalse(step_1.equals(aux2));
+		assertFalse(step_2.equals(aux1));
+		assertFalse("CMAS: 4\nsystems: 3\nCMASsystems: 1\ngood: 2\ninteger: 10".equals(s2.counter(step_1)));
+		
+		
 	}
 
 }
